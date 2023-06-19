c4cd81b2475d819f8786a87d643cc78dab54abe6
mezeiser <mezardini@gmail.com>
2023-06-19 17:28:51 +0100

    first commit

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  outputDir: 'dist', // Change 'dist' to your desired output directory
+};
